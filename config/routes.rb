8eb8bc192d7a1baacd1b27d240bdfbec34763286
Thainara Carvalho <thai.cds1@gmail.com>
2023-11-18 23:19:54 -0300

    teste

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "livros#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :livros
+  resources :book_reviews, path: 'classificacao', only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :home
+  resources :livros do
+    member do
+      get 'avaliar'
+      post 'save_avaliacao'
+      get 'desfavoritar', to: 'livros#desfavoritar'
+      get 'favoritar', to: 'livros#favoritar'
+    end
+  end
+  
+
+  devise_for :users, controllers: { registrations: 'users' }
+
+  get 'custom_logout', to: 'custom_sessions#destroy'
+  get 'todos_livros', to: 'livros#todos_livros'
+  get 'registered_book', to: 'livros#registered_book'
+  get 'meus_favoritos', to: 'favorites#index', as: 'meus_favoritos'
+
 end
